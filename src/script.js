5600b0dad62a2f57ec284606a7c2a830cc2e35b2
Siddharta Mohapatra <siddharthmohapatra2004@gmail.com>
2025-10-21 23:33:16 +0530

    update code files

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
-const toggleBtn = document.querySelector(".toggle-button")
-const dropMenu = document.querySelector(".dropdown-menu")
 
-toggleBtn.addEventListener('click', () =>{
+const dropMenu = document.querySelector('.dropdown-menu')
+function ontoggleMenu(e) {
+    e.name = e.name === 'menu' ? 'close' : 'menu'
     dropMenu.classList.toggle('top-20')
-})
+     
+}
